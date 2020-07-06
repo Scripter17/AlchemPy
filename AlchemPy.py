da7f6c99c3d578a50d036ad1575b92cb992f6caf
Scripter17 <jameschristopherwise@gmail.com>
2020-07-06 07:25:42 -0400

    Bet you fuckers thought this project was dead
    Well, it still kind of is
    And you probably weren't thinking of this project at all
    But still
    New commit

--- AlchemPy.py
+++ AlchemPy.py
@@ -30,14 +30,22 @@ for (x in bases.base){
 }
 """
 
+import os
 
-import msvcrt, os, colorama
-os.system("title AlchemPy - Little Alchemy in Python")
-colorama.init() # To move the cursor back to the end of the input
+os.system("cls")
 
-cstr="\033[2J\033[H"
-def clear():
-	print(cstr, end="")
+print("""
+AlchemPy - Little Alchemy in Python!
+       (c) James C. Wise 2020
+  Released under the DBaD license.
+       github.com/Scripter17/
+
+Left/Right -> Move through element list pages
+Type a combination of known elements like 'elem1+elem2',
+then press enter to submit. If it's a valid combination,
+it'll show up in the element list!"""[1:]) # It just looks good
+
+os.system("pause >nul && cls")
 
 data=open("Recipes.dat", "r").read()
 elements, recipes=tuple(data.split("\n-\n"))
@@ -45,34 +53,19 @@ elements=elements.split("\n")
 r=[]
 rkeys=[]
 for x in recipes.split("\n"):
+	# What in the goddamn fuck is any of this
 	com=set(x.split("=")[0].split("+"))
 	res=[x.split("=")[1]]
 	if com in rkeys:
-		r[[x[0] for x in r].index(com)][1].append(res[0])
+		r[[x[0] for x in r].index(com)][1].append(res[0]) # ?????
 	else:
 		rkeys.append(com)
 		r.append([com, res]) # [[{"earth, air"},["dust"]]...]
 recipes=r
 
-print("""\033[2J\033[H
-AlchemPy - Little Alchemy in Python!
-       (c) James C. Wise 2019
-  Released under the DBaD license.
-   \033[36mhttps://github.com/Scripter17/\033[39m
-
-Left/Right -> Move through element list pages
-Type a combination of known elements like 'elem1+elem2',
-then press enter to submit. If it's a valid combination,
-it'll show up in the element list!
-""", end="")
-msvcrt.getch()
-clear()
-
 dispPage=0 # Which 8 elements to list
 ePerPage=8 # How many elements to list per page
 txt=""
-cin=""
-cin2=""
 new=""
 while True:
 	#print(elements)
@@ -83,43 +76,26 @@ while True:
 	print("---")
 	print("\n".join(plist)+"\n"*(ePerPage-len(plist)))
 	print("---")
-	# If I do print(txt, end=""), the text will appear at the top for some unknown reason
-	# TODO: Fix this
 	print(txt)
 	if new!="":
-		print("Discovered "+new+"\033[1A")
-	print("\033[1A\033["+str(len(txt))+"C",end="")
-	cin=msvcrt.getch()
-	new=""
-	if cin==b"\xe0": # The user pressed an arrow key
-		cin2=msvcrt.getch()
-		if cin2==b"K": # The user pressed left
-			dispPage=max(dispPage-1, 0)
-		elif cin2==b"M": # The user pressed right
-			dispPage=min(dispPage+1, pages)
-	elif cin==b"\x08": # Backspace
-		txt=txt[:-1]
-	elif cin==b"\r": # Enter
-		com=txt.lower()
-		"""if (com not in recipes) and ("+".join(com.split("+")[::-1]) in recipes):
-			# If a+b isn't a recipe, but b+a is, do b+a
-			# earth+air = air+earth
-			com="+".join(com.split("+")[::-1])
-		if com in recipes and com.split("+")[0] in elements and com.split("+")[1] in elements:
-			# Append to discovered elements
-			elements.append(recipes[com])"""
-		if com=="exit":
-			exit()
-		else:
-			com=set(com.split("+"))
-			for x in recipes:
-				if x[0]==com:
-					new=", ".join(x[1])
-					for y in x[1]:
-						elements.append(y)
-		txt=""
+		print("Discovered "+new)
+	cin=input(">>>")
+	if cin in ["next", ">", "+", "."]:
+		dispPage+=1
+		if dispPage*ePerPage>len(plist):
+			dispPage=0
+	elif cin in ["previous", "prev", "last", "<", "-", ",", "just fucking go back you piece of shit game"]:
+		dispPage-=1
+		if dispPage<0:
+			dispPage=len(plist)//ePerPage
+	elif cin=="exit":
+		exit()
 	else:
-		allowedChars="abcdefghijklmnopqrstuvwxyz+- "
-		if cin.decode().lower() in allowedChars:
-			txt+=cin.decode()
-	clear()
+		cin=set(cin.split("+"))
+		for x in recipes:
+			if x[0]==cin:
+				new=", ".join(x[1])
+				for y in x[1]:
+					if not y in elements:
+						elements.append(y)
+	os.system("cls")
